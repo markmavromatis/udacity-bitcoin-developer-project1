3117124499fbf2252c40c1db41d100c2582dbc5e
Mark Mavromatis <mmavromatis@dii-102672.fios-router.home>
2022-12-14 15:56:48 -0500

    Imlemented Block validate() and getBData() methods

--- src/block.js
+++ src/block.js
@@ -39,15 +39,21 @@ class Block {
         let self = this;
         return new Promise((resolve, reject) => {
             // Save in auxiliary variable the current block hash
+            const currentBlockHash = self.hash;
                                             
             // Recalculate the hash of the Block
+            const newHashForComparison = SHA256(self.body);
             // Comparing if the hashes changed
             // Returning the Block is not valid
-            
-            // Returning the Block is valid
-
+            if (currentBlockHash != newHashForComparison) {
+                resolve(false);
+            } else {
+                // Returning the Block is valid
+                resolve(true);
+            }
         });
     }
+
 
     /**
      *  Auxiliary Method to return the block body (decoding the data)
@@ -59,12 +65,25 @@ class Block {
      *     or Reject with an error.
      */
     getBData() {
-        // Getting the encoded data saved in the Block
-        // Decoding the data to retrieve the JSON representation of the object
-        // Parse the data to an object to be retrieve.
+        let self = this;
+        return new Promise((resolve, reject) => {
 
-        // Resolve with the data if the object isn't the Genesis block
+            // Getting the encoded data saved in the Block
+            const blockEncodedData = self.body;
 
+            // Decoding the data to retrieve the JSON representation of the object
+            const decodedDataAsString = hex2ascii(blockEncodedData)
+
+            // Parse the data to an object to be retrieve.
+            const decodedObject = JSON.parse(decodedDataAsString)
+
+            // Resolve with the data if the object isn't the Genesis block
+            if (decodedObject.height == 0) {
+                reject("genesis block")
+            } else {
+                resolve(decodedObject);
+            }
+        })
     }
 
 }
